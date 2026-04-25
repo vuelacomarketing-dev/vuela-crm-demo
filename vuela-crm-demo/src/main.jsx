adf56b7ec47cd498989a0444c73b8d630d5d9ca2
vuelacomarketing-dev <vuelacomarketing@gmail.com>
2026-04-24 18:16:46 -0700

    Update main.jsx

--- vuela-crm-demo/src/main.jsx
+++ vuela-crm-demo/src/main.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import React, { useEffect, useState } from "react";
 import {
   Rocket,
   LayoutDashboard,
